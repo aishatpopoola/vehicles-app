00b8499174be395892605e7d358d4da611422b60
aishatpopoola <78761789+aishatpopoola@users.noreply.github.com>
2021-10-02 08:03:07 +0100

    add needed components

---
+++ src/components/Vehicle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Vehicle = () => (
+  <div>
+    <h2>Vehicle</h2>
+  </div>
+);
+
+export default Vehicle;
---
+++ src/components/Vehicles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Vehicles = () => (
+  <div>
+    <h2>Vehicles</h2>
+  </div>
+);
+
+export default Vehicles;
